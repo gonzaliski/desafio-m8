30489f4f3b444c103308a3a476bccb7e48275ab4
Gon <gon.mendez13@gmail.com>
2022-09-19 12:06:09 -0300

    firebase json and prettier

--- src/ui/buttons/index.tsx
+++ src/ui/buttons/index.tsx
@@ -1,14 +1,26 @@
-import React from "react"
-import css from "./buttons.css"
+import React from "react";
+import css from "./buttons.css";
 
-export function MainButton(props){
-    return <button onClick={props.onClick} className={css['main']}>{props.children}</button>
+export function MainButton(props) {
+  return (
+    <button onClick={props.onClick} className={css["main"]}>
+      {props.children}
+    </button>
+  );
 }
 
-export function SecondaryButton(props){
-    return <button onClick={props.onClick} className={css['secondary']}>{props.children}</button>
+export function SecondaryButton(props) {
+  return (
+    <button onClick={props.onClick} className={css["secondary"]}>
+      {props.children}
+    </button>
+  );
 }
 
-export function BorderButton(props){
-    return <button onClick={props.onClick} className={css['border']}>{props.children}</button>
+export function BorderButton(props) {
+  return (
+    <button onClick={props.onClick} className={css["border"]}>
+      {props.children}
+    </button>
+  );
 }
