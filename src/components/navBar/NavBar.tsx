24c7c392983c6e051294adada2d296b03b2f47b8
Gon <gon.mendez13@gmail.com>
2022-09-22 10:58:38 -0300

    navbar fix

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -34,6 +34,7 @@ export function NavBar() {
 
   const logOut = ()=>{
     handleClick()
+    setUserInfoClicked(!userInfoClicked)
     setUserData({})
     setToken("")
     navigate("/",{replace:true})
